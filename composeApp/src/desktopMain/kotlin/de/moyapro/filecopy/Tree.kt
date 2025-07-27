f075690eade4a6830c057af4795db7aa5a5dec18
Tom Ritmeier <tom.ritmeier@iits-consulting.de>
2025-07-27 11:38:19 +0200

    select files

--- composeApp/src/desktopMain/kotlin/de/moyapro/filecopy/Tree.kt
+++ composeApp/src/desktopMain/kotlin/de/moyapro/filecopy/Tree.kt
@@ -1,7 +1,10 @@
 package de.moyapro.filecopy
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -9,6 +12,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
+import de.moyapro.filecopy.model.FileSystemNode
 
 @Composable
 fun Tree(rootDir: java.io.File) {
@@ -21,8 +25,29 @@ fun Tree(rootDir: java.io.File) {
     ) {
         LazyColumn(modifier = Modifier) {
             items(items = nodes, key = { it.hashCode() }) { node ->
-                if (node.isVisible) Text(node.text())
+                AnimatedVisibility(node.isVisible) {
+                    Row(modifier = Modifier.fillMaxWidth()) {
+                        Text(
+                            text = (if (node.isSelected) "[✔]" else "[]"),
+                            modifier = Modifier.clickable(onClick = { nodes.replace(node.copy(isSelected = !node.isSelected)) })
+                        )
+                        Text((if (node.isDirectory()) "[+]" else "♬"))
+                        Text(node.text())
+                    }
+                }
             }
         }
     }
+}
+
+fun MutableList<FileSystemNode>.replace(elementToReplace: FileSystemNode) {
+    var index = -1
+    for (i in indices) {
+        if (this[i].id == elementToReplace.id) {
+            index = i
+            break
+        }
+    }
+    require(index >= 0) { "Element to replace is not in the list." }
+    set(index, elementToReplace)
 }
--- composeApp/src/desktopMain/kotlin/de/moyapro/filecopy/model/FileSystemNode.kt
+++ composeApp/src/desktopMain/kotlin/de/moyapro/filecopy/model/FileSystemNode.kt
@@ -1,10 +1,14 @@
 package de.moyapro.filecopy.model
 
 import java.io.File
+import java.util.*
 
-class FileSystemNode(val file: File) {
-    var isSelected: Boolean = false
-    var isVisible: Boolean = false
+data class FileSystemNode(
+    val file: File,
+    var isSelected: Boolean = false,
+    var isVisible: Boolean = true,
+    val id: UUID = UUID.randomUUID(),
+) {
     fun isDirectory() = file.isDirectory
     fun text(): String = file.absolutePath
 }
